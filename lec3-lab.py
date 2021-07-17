48ba50b9a974b06ae484816cd53a926cf3979e9a
mkinney-mitre <85962924+mkinney-mitre@users.noreply.github.com>
2021-07-17 08:39:13 -0400

    Create lec3-lab.py

---
+++ lec3-lab.py
@@ -0,0 +1,29 @@
+# 1.
+# Manipulate the string variable s to output the correct variations.
+s = "6.00 is 6.0001 and 6.0002"
+
+# a. Manipulate s to print out '62'
+# print(s[...] + s[...])
+
+# b. Manipulate s to print out every other character starting in position 4
+# print(s[...:...:...])
+
+# c. Manipulate s to print out the characters in positions 2 - 10 backwards
+# print(s[...:...][...:...:...])
+
+# 2.
+# Complete this double for loop
+# s = "mit u rock"
+
+
+# TODO for loop over the character in the 1st position through the end of the string s
+# for ...:
+  # TODO for loop over the character in the 0th position through the second from the last position in the string
+#   for ...:
+    # TODO Check if char1 comes before char2 in the alphabet
+#     if ...:
+      # TODO Print out char1 comes first and print the value of char1
+#     else:
+      # TODO Print out char2 comes first and print the value of char2
+
+# Hint: To get the numeric position of the letter in the alphabet use the ord() function
